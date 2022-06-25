03b1eabc6e12be930f195f2e40b9677277309e3f
AlperK <alperkilic93gmail.com>
2022-06-25 13:34:47 -0400

    Implemented event_handler() Started implemented dds_events()

--- events.py
+++ events.py
@@ -5,24 +5,30 @@ def event_handler(app, event, values):
     if event == Sg.WIN_CLOSED:
         return -1
     else:
-        event_group = event.split('_')[0]
+        event_group = event.split('__')[1].split('_')[0]
+        value = values[event]
+        print(f'Event Group: {event_group}')
+        print(f'Event Value: {value}')
 
         if event_group == 'DDS':
-            values = values['DDS_SETTINGS']
-            dds_events(app, event, values)
+            value = values[event]
+            dds_events(app, event, value)
 
 
-def dds_events(app, event, values):
-    if event == 'DDS_P_DWN':
-        if values['PDWN']:
+def dds_events(app, event, value):
+    if event == '__DDS_P_DWN__':
+        if value:
             app.dds.shutdown()
         else:
             app.dds.wake_up()
 
-    elif event == 'DDS_RST':
+    elif event == '__DDS_RST__':
         app.dds.reset()
-        app.dds.load_settings(settings=values)
+        app.dds.load_settings(settings=value)
 
-        app['RF'].update(value=values['RF'])
-        app['IF'].update(value=values['IF'])
-        app['PLL_MUL'].update(value=values['PLL_MUL'])
+        app['RF'].update(value=value)
+        app['IF'].update(value=value)
+        app['PLL_MUL'].update(value=value)
+
+    else:
+        print(f'Event: {event}')
