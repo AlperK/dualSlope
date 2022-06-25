eb759c9f61bc27aef243760d59f2da48cd87bf92
AlperK <alperkilic93gmail.com>
2022-06-25 12:56:23 -0400

    Added the Channel Enable checkboxes. Resized the Frequency Frame.

--- ui/windows2.py
+++ ui/windows2.py
@@ -11,22 +11,24 @@ class HardwareTab(Sg.Tab):
     def __init__(self):
         self.layout = [[DDSFrame()]]
         super(HardwareTab, self).__init__(title=f'Hardware \n Tab',
-                                          layout=self.layout,)
+                                          layout=self.layout, )
 
 
 class DDSFrame(Sg.Frame):
     def __init__(self):
         _CHA_FRE_ROW = [
             [Sg.Text(text='RF', size=(15, None)),
-            Sg.Input(default_text=DDS_SETTINGS['RF'], size=(15, None), key='__DDS_RF__'),
-            Sg.Text(text='IF', size=(15, None)),
-            Sg.Input(default_text=DDS_SETTINGS['IF'], size=(15, None), key='__DDS_IF__'),
-        ]
+             Sg.Input(default_text=DDS_SETTINGS['RF'], size=(15, None), key='__DDS_RF__'),
+             Sg.Text('', size=(8, None)),
+             Sg.Text(text='CF', size=(15, None)),
+             Sg.Input(default_text=DDS_SETTINGS['IF'], size=(15, None), key='__DDS_IF__'),
+             ]
         ]
         _CHA_FRE_FRA = Sg.Frame(title='DDS Frequency Settings',
-                                layout=_CHA_FRE_ROW)
+                                layout=_CHA_FRE_ROW,
+                                size=(610, 50))
 
-        _CHA_AMP_ROW = [
+        _CHA_AMP_COL = [
             ([
                 Sg.Text(text=f'Channel {channel}',
                         size=(15, None)),
@@ -37,28 +39,44 @@ class DDSFrame(Sg.Frame):
             ])
             for channel in range(4)]
         _CHA_AMP_FRA = Sg.Frame(title='DDS Amplitude Settings',
-                                layout=_CHA_AMP_ROW)
+                                layout=_CHA_AMP_COL,
+                                size=(300, 135))
 
-        _CHA_PHA_ROW = [
-            ([
+        _CHA_PHA_COL = [
+            [
                 Sg.Text(text=f'Channel {channel}',
                         size=(15, None)),
                 Sg.InputText(default_text=f'{channel}',
                              size=(15, None),
                              enable_events=True,
                              key=f'__DDS_CHA_PHA__{channel}')
-            ])
+            ]
             for channel in range(4)]
         _CHA_PHA_FRA = Sg.Frame(title='DDS Phase Settings',
-                                layout=_CHA_PHA_ROW)
+                                layout=_CHA_PHA_COL,
+                                size=(300, 135))
+
+        _CHA_EN_COL = [
+            [
+                Sg.Checkbox(text='Enable',
+                            default=True,
+                            key=f'__DDS_CHA_EN__{channel}',
+                            enable_events=True)
+            ]
+            for channel in range(4)
+        ]
+        _CHA_EN_FRA = Sg.Frame(title='Channel Enable',
+                               layout=_CHA_EN_COL,
+                               size=(300, 135))
+
         self.layout = [
             [
                 Sg.Column(layout=[
-                          [_CHA_FRE_FRA],
-                          [_CHA_AMP_FRA,
-                           _CHA_PHA_FRA]])
+                    [_CHA_FRE_FRA],
+                    [_CHA_AMP_FRA,
+                     _CHA_PHA_FRA,
+                     _CHA_EN_FRA]])
             ]
         ]
 
         super().__init__(title='DDS Settings', layout=self.layout)
-
