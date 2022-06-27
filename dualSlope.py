52e83cd234a89e82030c6936f0714611020d8af0
AlperK <alperkilic93gmail.com>
2022-06-27 11:24:37 -0400

    event_handler() addition.

--- dualSlope.py
+++ dualSlope.py
@@ -1,6 +1,7 @@
 import ui.app as app
 import RPi.GPIO as GPIO
 import PySimpleGUI as Sg
+from events import event_handler
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -9,8 +10,8 @@ app = app.MainApplication()
 while True:
     event, values = app.read(timeout=0)
     if event not in ['__TIMEOUT__', Sg.WIN_CLOSED]:
-        print(f'event: {event}, value: {values[event]}')
-    # a = UI.event_values(app, event, values)
+        # print(f'event: {event}, value: {values[event]}')
+        a = event_handler(app, event, values)
     # print(event)
     # if a == -1:
     #     break
