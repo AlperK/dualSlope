8aa0dc127aa431bc4fbbdd3ee6c463a874fc43be
AlperK <alperkilic93gmail.com>
2022-07-04 12:17:14 -0400

    Typo fix.

--- ui/laser.py
+++ ui/laser.py
@@ -2,15 +2,15 @@ import PySimpleGUI as Sg
 
 _LAS_COL = Sg.Column(
     layout=[
-    [Sg.Text(f"Laser-{i}: "), Sg.Checkbox(text='On/Off',
-                                          default=False,
-                                          key=f"__LAS__{i}",
-                                          enable_events=True,
-                                          )
-     ]
-    for i in range(1, 5)
-],
-    pad=(10, (10, 0)),)
+        [Sg.Text(f"Laser-{i}: "), Sg.Checkbox(text='On/Off',
+                                              default=False,
+                                              key=f"__LAS__{i}",
+                                              enable_events=True,
+                                              )
+         ]
+        for i in range(1, 5)
+    ],
+    pad=(10, (10, 0)), )
 
 laser_frame = Sg.Frame(
     title='Laser Controls',
