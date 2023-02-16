fd6cac34d8570410aafc2646e79b41a110bf2cf0
AlperK <alperkilic93@gmail.com>
2023-02-16 16:47:17 -0500

    Minor syntax warning fix.

--- funcs/las_funcs.py
+++ funcs/las_funcs.py
@@ -33,7 +33,7 @@ def turn_off_all_lasers():
     active_pins = []
     for i in range(1, 41):
         if i not in [1, 2, 4, 6, 9, 14, 17, 20, 25, 27, 28, 30, 34, 39]:
-            if GPIO.gpio_function(i) is not -1:
+            if GPIO.gpio_function(i) != -1:
                 active_pins.append(i)
     cleanup_pins = [pin for pin in active_pins if pin not in [10, 13, 15, 16]]
     GPIO.cleanup(cleanup_pins)
